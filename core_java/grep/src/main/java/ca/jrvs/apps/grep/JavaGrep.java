0510d35652f67217a014587da3cc2801e7c08186
unknown <nariman.alimuradov@gmail.com>
2021-03-31 17:33:37 -0400

    Core Java Regex - basic regex Java complete

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
@@ -0,0 +1,51 @@
+package ca.jrvs.apps.grep;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public interface JavaGrep {
+
+    /**
+     * Top level search workflow
+     * @throws IOException
+     */
+    void process() throws IOException;
+
+    /**
+     * List all files in a given directory
+     * @param rootDir input directory
+     * @return list of files in the directory
+     */
+    List<File> listFiles(String rootDir);
+
+    /**
+     * Read lines from a file
+     * @param inputFile file being read from
+     * @return lines that are read
+     * @throws IllegalArgumentException if incorrect input file format
+     */
+    List<String> readLines(File inputFile) throws IOException;
+
+    /**
+     * Check if line contains the regex pattern passed
+     * @param line input string
+     * @return true if there is a match, false otherwise
+     */
+    boolean containsPattern(String line);
+
+    /**
+     * Write to a file
+     * @param lines
+     * @throws IOException
+     */
+    void writeToFile(List<String> lines) throws IOException;
+
+    // getters and setters
+    String getRootPath();
+    void setRootPath(String rootPath);
+    String getRegex();
+    void setRegex(String regex);
+    String getOutFile();
+    void setOutFile(String outFile);
+}
---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,115 @@
+package ca.jrvs.apps.grep;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaGrepImp implements JavaGrep {
+
+    Logger parentLogger = LogManager.getLogger(JavaGrep.class);
+    private Logger logger = parentLogger;
+    private String regex;
+    private String rootPath;
+    private String outFile;
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Must provide: regex rootPath outFile");
+        }
+
+        BasicConfigurator.configure();
+        JavaGrepImp javaGrepImp = new JavaGrepImp();
+        javaGrepImp.setRegex(args[0]);
+        javaGrepImp.setRootPath(args[1]);
+        javaGrepImp.setOutFile(args[2]);
+
+        try {
+            javaGrepImp.process();
+        } catch (Exception e) {
+            // logger.error(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void process() throws IOException {
+        List<String> matchedLines = new ArrayList<String>();
+        for (File file : listFiles(rootPath)) {
+            for (String line : readLines(file)) {
+                if (containsPattern(line)) {
+                    matchedLines.add(line);
+                }
+            }
+        }
+        writeToFile(matchedLines);
+    }
+
+    @Override
+    public List<File> listFiles(String rootDir) {
+        List<File> validFiles = new ArrayList<File>();
+        File[] allFiles = new File(rootDir).listFiles();
+        for (File f : allFiles) {
+            if (f.isFile()){
+                validFiles.add(f);
+            }
+        }
+        return validFiles;
+    }
+
+    @Override
+    public List<String> readLines(File inputFile) throws IOException {
+        List<String> fileLines = Files.readAllLines(new File(rootPath + "/" + inputFile.getName()).toPath());
+        return fileLines;
+    }
+
+    @Override
+    public boolean containsPattern(String line) {
+        if (line.matches(regex)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void writeToFile(List<String> lines) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(rootPath + "/" + outFile));
+        for (String line : lines) {
+            writer.write(line);
+        }
+        writer.close();
+    }
+
+    @Override
+    public String getRootPath() {
+        return this.rootPath;
+    }
+
+    @Override
+    public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    @Override
+    public String getRegex() {
+        return this.regex;
+    }
+
+    @Override
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    @Override
+    public String getOutFile() {
+        return this.outFile;
+    }
+
+    @Override
+    public void setOutFile(String outFile) {
+        this.outFile = outFile;
+    }
+}
