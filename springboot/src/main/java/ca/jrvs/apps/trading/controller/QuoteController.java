5d8b861991200776145a8be19877e1efa03a84cc
Nariman Alimuradov <nariman.alimuradov@gmail.com>
2021-05-25 19:18:47 +0000

    springboot fix

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping
+@RequestMapping("/quote")
 public class QuoteController {
 
     private QuoteService quoteService;
